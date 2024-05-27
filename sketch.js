b828a64833a9ab9af8f8e750645fce212c873c18
lujonunes <49663814+lujonunes@users.noreply.github.com>
2024-05-27 16:03:50 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,8 @@
+let nome = "Maria";
+let idade = 25;
+function setup() {
+  const identificacao = [ "A "  + nome + " tem " + idade + " anos "]
+  console.log(identificacao);
+                        
+}
+
